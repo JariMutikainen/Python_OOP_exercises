812430611e20f77034587a44a0dbcde477d9684f
Jari Mutikainen <jari.mutikainen@welho.net>
2019-01-24 17:47:06 +0200

    Completed the pytest exercise.

--- chapter7/test_conf_dict.py
+++ chapter7/test_conf_dict.py
@@ -2,17 +2,34 @@
 # pytest for unittesting.
 import pytest
 from conf_dict import ConfigDict, ConfigKeyError
-import subprocess
+import shutil
+import os
 
 class TestConfigDict:
 
     safety_file = './safety_copy.json'
-    data_file_in_use = './data.json'
+    data_file_in_use = './temporary_data.json'
 
     def setup_class(self):
-        subprocess.call('cp', safety_file, data_file_in_use)
+        shutil.copyfile(TestConfigDict.safety_file,
+                    TestConfigDict.data_file_in_use)
+        self.cd = ConfigDict(TestConfigDict.data_file_in_use)
 
     def teardown_class(self):
-        pass
+        os.remove(TestConfigDict.data_file_in_use)
+
+    def test_obj(self):
+        assert isinstance(self.cd, ConfigDict)
+        assert isinstance(self.cd, dict)
         
+    def test_filename_stored(self):
+        assert self.cd.filename == TestConfigDict.data_file_in_use
+
+    def test_insert_new_item(self):
+        self.cd['some_key'] = 'some_value'
+        assert self.cd['some_key'] == 'some_value'
+
+    def test_illegal_key(self):
+        with pytest.raises(ConfigKeyError):
+            print(self.cd['non_existing_key'])
 
